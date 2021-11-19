ea332383c5a2df420edf0d48c676b91176eeab28
Sumit Singh <sumitbhavra@gmail.com>
2021-11-19 12:06:48 +0530

    solved problem

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,35 @@
 const { httpGet } = require('./mock-http-interface');
 
-const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+const HTTP = {
+  "STATUS_OK": 200
+}
+
+Object.freeze(HTTP);
+
+const Message = (type, body) => {
+  return {
+    [type]: JSON.parse(body).message
+  }
+}
+
+/**
+ * @param {Array} URLS to call
+ * @returns {Promise} promise resolves to results array.
+ */
+const getArnieQuotes = async (urls = []) => {
+  const resolvers = urls.map(async (url) => {
+    try {
+      const { status, body } = await httpGet(url);
+      if (status === HTTP.STATUS_OK) {
+        return Message('Arnie Quote', body)
+      }
+      return Message('FAILURE', body)
+    } catch (error) {
+      return Message('FAILURE', error);
+    }
+  });
+
+  return Promise.all(resolvers);
 };
 
 module.exports = {
